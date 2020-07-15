fbb179a0b0d34bd63eedff98bf609ce0a2108e97
Michelle Colin <michelle.c.colin@gmail.com>
2020-07-14 23:45:42 -0300

    Create components

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DeckListComponent } from './deck-list/deck-list.component';
+import { DeckComponent } from './deck/deck.component';
+import { PokemonComponent } from './pokemon/pokemon.component';
+
+const appRoutes: Routes = [
+    { path: '', redirectTo: 'deck-list', pathMatch: 'full'},
+    { path: 'deck-list', component: DeckListComponent},
+    { path: 'deck/:id', component: DeckComponent},
+    { path: 'pokemon/:id', component: PokemonComponent }
+  ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DeckListComponent } from './deck-list/deck-list.component';
+import { DeckComponent } from './deck/deck.component';
+import { PokemonComponent } from './pokemon/pokemon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeckListComponent,
+    DeckComponent,
+    PokemonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
